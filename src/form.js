c591fdd07825014f791e04483fd6ce55b9ee5e71
badr ghanbi <bghanbi50@gmail.com>
2022-11-01 16:09:45 +0100

    set up react app

---
+++ src/form.js
@@ -0,0 +1,11 @@
+function Form(){
+    return(
+        <form className="form">
+         <input placeholder="Enter Name" />
+          <button>Submit</button>
+        </form>
+    )
+}
+
+
+export default Form;
