b00da74879ad3d7f8b2693b7c5cf0150472495f7
badr ghanbi <bghanbi50@gmail.com>
2022-11-02 16:52:44 +0100

    add the functionality to the app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import Balance from "./component/balance";
 import Spendings from './component/spendings';
-
+import Total from "./component/total";
 function App() {
   return (
     <div className="container">
        <Balance/>
        <Spendings/>
+       <Total/>
     </div>
   );
 }
