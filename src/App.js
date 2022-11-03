db3ee2712c5501e9f74a386437ddbefb683b7d38
badr ghanbi <bghanbi50@gmail.com>
2022-11-03 12:47:23 +0100

    clean up work environment

--- src/App.js
+++ src/App.js
@@ -1,14 +1,5 @@
-import Balance from "./component/balance";
-import Spendings from './component/spendings';
-import Total from "./component/total";
 function App() {
-  return (
-    <div className="container">
-       <Balance/>
-       <Spendings/>
-       <Total/>
-    </div>
-  );
+ 
 }
 
 export default App;
