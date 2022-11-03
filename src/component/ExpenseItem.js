0f0029faf2d692ac16ebab2a7af4d61892861ae1
badr ghanbi <bghanbi50@gmail.com>
2022-11-03 15:05:20 +0100

    add sass to our project

--- src/component/ExpenseItem.js
+++ src/component/ExpenseItem.js
@@ -1,10 +1,11 @@
+
 function ExpenseItem() {
     return (
-        <>
+        <div className="expenseItem">
             <h2>Expense item</h2>
             <p>Hello world</p>
             <div>its a div</div>
-        </>
+        </div>
     )
 
 }
