36da38f62159881fdcd6d4b58393077b5b6d773a
badr ghanbi <bghanbi50@gmail.com>
2022-11-04 14:16:39 +0100

    add create the typography section

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 
 function App() {  
  return(
-  <h1>Hello</h1>
+  <>
+  <h1>The Bright Future of Web 3.0?</h1>
+  <h2>Hydrogen VS Electric Cars</h2>
+  <h3>Reviving Retro PCs</h3>
+   <p>
+    We dive into the next evolution of the web that claims to put the power of the platforms back into the hands of the people. But is it really fulfilling its promise?
+   </p>
+  </>
  )
 }
 
