97ce88405131e8ae70b4e23c7b456fbf5c35b948
badr ghanbi <bghanbi50@gmail.com>
2022-11-05 19:08:49 +0100

    creating the nav component

---
+++ src/component/nav.js
@@ -0,0 +1,29 @@
+import menu from '../images/icon-menu.svg';
+import menuClose from '../images/icon-menu-close.svg';
+import logo from '../images/logo.svg';
+import { useState } from 'react';
+
+
+function Nav() {
+  const [isOpen, setIsOpen] = useState(false);
+  const handleClick = () => setIsOpen(!isOpen);
+  const ListState= isOpen ? 'show':'' ;
+  const menuState= isOpen ? 'hide':'' ;
+  return (
+    <div className='nav'>
+      <img className='nav__logo' src={logo} />
+      <img onClick={handleClick} className={`${menuState} nav__menu`}  src={menu} />
+      <img onClick={handleClick} className={`${ListState} nav__menuClose`} src={menuClose} />
+      <ul  className= {`${ListState} nav__list`}>
+        <li className='nav__list-link'>Home</li>
+        <li className='nav__list-link'>new</li>
+        <li className='nav__list-link'>popular</li>
+        <li className='nav__list-link'>trending</li>
+        <li className='nav__list-link'>categories</li>
+      </ul>
+    </div>
+  )
+}
+
+
+export default Nav;
