3a004b4d6b06e92bbd579e56e7b4174c696a825b
badr ghanbi <bghanbi50@gmail.com>
2022-11-08 12:15:39 +0100

    build for production

--- src/App.js
+++ src/App.js
@@ -11,12 +11,16 @@ function App() {
     <>
      <Nav/>
      <Hero/>
+     <div className="News-container">
      <News title="Hydrogen VS Electric Cars" text='Will hydrogen-fueled cars ever catch up to EVs?'/>
      <News title="The Downsides of AI Artistry" text='What are the possible adverse effects of on-demand AI image generation?'/>
      <News title="Is VC Funding Drying Up?" text='Private funding by VC firms is down 50% YOY. We take a look at what that means.'/>
+     </div>
+     <div className="features-container">
      <Features number="01" src={image1}/>
      <Features number="02" src={image2}/>
      <Features number="03" src={image3}/>
+     </div>
     </>
  )
 }
